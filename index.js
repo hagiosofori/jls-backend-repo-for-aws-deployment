5498841d1ea0694be2c98db7bac150b8ad0b07ac
Seyram <alvinofori@expresspaygh.com>
2021-05-02 16:06:09 +0000

    add text to show when the home endpoint is hit

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 // import db from './db';
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const { db } = require('./db');
-const {productRoutes, productLocationRoutes} = require('./routes');
+const { productRoutes, productLocationRoutes } = require('./routes');
 
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/products', productRoutes)
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', (req, res) => { res.status(200).json({ message: "Welcome to Alvin's API for the JLS assessment" }); });
+app.use('/products', productRoutes);
 app.use('/product-locations', productLocationRoutes);
 
 app.listen(process.env.PORT, async () => {
